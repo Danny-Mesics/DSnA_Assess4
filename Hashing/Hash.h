54cb6afbd7251ac31ad8efd4a9b3e13eed0ae587
Danny-Mesics <cybertomasvrbada@gmail.com>
2023-09-01 19:32:00 +1000

    Hashing assignment done
    If there is time come back to the resource manager

---
+++ Hashing/Hash.h
@@ -0,0 +1,17 @@
+#pragma once
+
+
+#include <functional>
+namespace HashFunction {
+
+    typedef std::function<unsigned int(const char*, unsigned int)> HashFunc;
+
+    // implementation of a basic addition hash
+    unsigned int badHash(const char* data, unsigned int length);
+
+    // ADD YOUR FUNCTIONS HERE
+    unsigned int MyHash(const char* data, unsigned int length);
+    
+    // a helper to access a default hash function
+    static HashFunc defaulted = MyHash;
+}
---
+++ Hashing/Hashing.cpp
@@ -0,0 +1,57 @@
+// Hashing.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "Hash.h"
+
+namespace HashFunction {
+
+    // implementation of a basic addition hash
+    unsigned int badHash(const char* data, unsigned int length)
+    {
+        unsigned int hash = 0;
+
+        for (unsigned int i = 0; i < length; ++i)
+            hash += data[i];
+
+        return hash;
+    }
+
+    unsigned int MyHash(const char* data, unsigned int length) 
+    {
+        //Using prime numbers seed all values
+
+        unsigned int hash = 7;
+        int evens = 2;
+        int odds = 3;
+
+        for (int i = 0; i < length; i++) {
+            if (data[i] % 2 == 0) {
+                evens += data[i];
+            }
+            else {
+                odds += data[i];
+            }
+        }
+
+        hash = ((evens * 11) - hash / (odds * 5) - (hash * 13)); 
+        
+
+        // Ensures hash will return a 5 digit int
+        while (hash < 10000 || hash > 100000) {
+            if (hash < 10000) {
+                hash = 10000 + (hash / 2);
+            }
+            if (hash >= 100000) {
+                hash = 100000 - (hash / 2);
+            }
+        }
+        
+        return hash;
+    }
+}
+
+int main() 
+{
+    std::cout << HashFunction::MyHash("Alright boys listen heres the play", 35) << std::endl;
+}
